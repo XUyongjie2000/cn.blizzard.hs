0f1c64e1ba2639234b7d64754d9bb6f11e097ef5
XuYongJie2000 <99706807@qq.com>
2020-04-24 21:43:12 +0800

    项目

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#fullpage").fullpage({
+    navigation: true,
+    scrollingSpeed: 1500,
+  });
+});
+$(".jianianhua").on("click", function () {
+  $(".Navbar-modalContent").toggle();
+});
