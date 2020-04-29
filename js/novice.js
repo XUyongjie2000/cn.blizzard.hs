f953cc6d9f1954edd725f6e53746fbf959e54910
XuYongJie2000 <99706807@qq.com>
2020-04-29 10:02:48 +0800

    项目

--- js/novice.js
+++ js/novice.js
@@ -47,3 +47,46 @@ $(".novice5").on("click", function () {
   $(".novice3").show();
   $(".novice2")[0].play();
 });
+// 点击圆点 到对应位置
+let slider = document.querySelector(".slider");
+let ul = document.querySelector(".slider ul");
+let ol = document.querySelector("ol");
+for (let i = 0; i < $(".slider ul").children().length; i++) {
+  let li = document.createElement("li");
+  // li[i].index = i;
+  if (i == 0) {
+    li.classList.add("active");
+  }
+  console.log(i);
+  ol.appendChild(li);
+  // li.addEventListener("click", function () {
+  //   // console.log(this);
+  //   for (let j = 0; j < ol.children.length; j++) {
+  //     ol.children[j].classList.remove("active");
+  //   }
+  //   this.classList.add("active");
+  //   let index = this.index;
+  //   console.log(this  );
+  // });
+}
+$("ol").on("click", "li", function () {
+  $(this).addClass("active").siblings().removeClass("active");
+  console.log($(this).index());
+  animate(ul, {
+    left: -$(this).index() * $(".slider ul li").width(),
+  });
+});
+console.log($(".slider ul").children());
+// var index = 0;
+// var timer = setInterval(auto, 8000);
+// function auto() {
+//   if (index == ul.children.length - 1) {
+//     ul.style.left = "0px";
+//     index = 0; //重复从第一张开始滚动
+//   }
+//   index++;
+//   $("ol li").eq(index).addClass("active").siblings().removeClass("active");
+//   animate(ul, {
+//     left: -index * $("ul>li").width(),
+//   });
+// }
