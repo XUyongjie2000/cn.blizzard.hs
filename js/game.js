3f4e2695fefae1fb844e4a1c96565515d7f44672
XuYongJie2000 <99706807@qq.com>
2020-04-29 19:27:17 +0800

    项目

---
+++ js/game.js
@@ -0,0 +1,36 @@
+$(".jianianhua").on("click", function () {
+  $(".Navbar-modalContent").toggle();
+});
+$(".Menu1").mouseenter(function () {
+  $(".Menu-dropdown1").show();
+});
+$(".Menu1").mouseleave(function () {
+  $(".Menu-dropdown1").hide();
+});
+$(".Menu2").mouseenter(function () {
+  $(".Menu-dropdown2").show();
+});
+$(".Menu2").mouseleave(function () {
+  $(".Menu-dropdown2").hide();
+});
+$(".Menu3").mouseenter(function () {
+  $(".Menu-dropdown3").show();
+});
+$(".Menu3").mouseleave(function () {
+  $(".Menu-dropdown3").hide();
+});
+$(".Menu4").mouseenter(function () {
+  $(".Menu-dropdown4").show();
+});
+$(".Menu4").mouseleave(function () {
+  $(".Menu-dropdown4").hide();
+});
+$(".Menu6").mouseenter(function () {
+  $(".Menu-dropdown6").show();
+});
+$(".Menu6").mouseleave(function () {
+  $(".Menu-dropdown6").hide();
+});
+$(".xiaoguo").on("click", function () {
+  $(".xianshi").toggle();
+});
